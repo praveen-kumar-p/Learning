ee97fe092dfd65d47b66d24c581d7ffe22413be3
Praveen Kumar P Iyli <0554@ymts.net>
2024-07-29 17:17:50 +0530

    Initial commit

---
+++ src/ex_Lab001/Prat001.java
@@ -0,0 +1,210 @@
+package ex_Lab001;
+
+import java.util.Scanner;
+
+public class Prat001 {
+    public static void main(String[] args) {
+
+//        System.out.println("Praveen");
+//        System.out.println("Iyli");
+
+//        int a = 10;
+//        System.out.println(++a + a++ + ++a);
+//        System.out.println(a);
+//
+//        int b = 20;
+//        System.out.println(++b + b-- + --b);
+//        System.out.println(b);
+
+        // Program to display numbers from 1 to 5
+//        int i = 1;
+//        while (i<=5){
+//            System.out.println("Numbers --> " + i);
+//            i++;
+//        }
+
+        //Write a program to print numbers from 1 to 10
+//        for (int i = 0; i <=10; ++i){
+//            System.out.println("Numbers " + i);
+//        }
+
+        //Write a program to calculate the sum of first 10 natural number.
+//        int sum = 0;
+//        for (int i = 0; i<=10; ++i){
+//            sum =+ i;
+//        }
+//        System.out.println(sum);
+
+        //Write a program to find the factorial value of any number entered through the keyboard.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the Numbers");
+//        int n = sc.nextInt();
+//        int fact = 1;
+//        for (int i = 1; i<=5; i++){
+//            fact = fact*i;
+//            System.out.println(fact);
+//        }
+
+//        int sum = 1;
+//        for (int i=1; i<5; i++){
+//            sum = sum +(2*i);
+//        }
+//        System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter natual numbers");
+//        int n = sc.nextInt();
+
+//        for (int i = 1; i <= n; i++){
+//            System.out.println(i);
+//        }
+
+//        for (int i = 10; i >= n; --i){
+//            System.out.println(i);
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter start: ");
+//        int n = sc.next().charAt(0);
+
+
+// write program to print all character from 'a' to 'z'
+//        for (char i = 'a'; i <= 'z'; i++){
+//            System.out.println(i);
+//        }
+
+//        write program to print all characters from 'z' to 'a'
+//        for (char i = 'z'; i >= 'a'; --i){
+//            System.out.println(i);
+//        }
+
+//        write program to print even numbers from 1 to 20
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Even Number");
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i <= n; i++)
+//        {
+//            if (i % 2 == 0)
+//                System.out.println(i);
+//        }
+
+//        write program to print odd numbers from 1 to 100
+//        Scanner o = new Scanner(System.in);
+//        System.out.println("Enter number");
+//        int n = o.nextInt();
+//
+//        for (int i = 0; i <= n; i++)
+//        {
+//            if (i%2 != 0) System.out.println(i);
+//        }
+
+
+//        Write program to print sum of even numbers
+//        int sum = 0;
+//        for (int i = 1; i <= 10; i++)
+//        {
+//            if (i%2 == 0)
+//                sum += i;
+//        }
+//        System.out.println(sum);
+
+//        write program to print sum of odd number
+//        int sum = 0;
+//        for (int i = 0; i <= 10; i++)
+//        {
+//            if (i%2!=0)
+//                sum += i;
+//        }
+//        System.out.println(sum);
+
+//        write program to print factorial number
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int n = sc.nextInt();
+//
+//        int fact = 1;
+//        for (int i=1; i<=n; i++)
+//        {
+//            fact = fact * i;
+//        }
+//        System.out.println(fact);
+
+//        int fact = 1;
+//        for (int i =1; i<=5; i++){
+//            fact = fact*i;
+//        }
+//        System.out.println(fact);
+
+//        Write a program to print the ASCII values
+//        for (int i = 32; i <= 126; i++) {
+//            // Convert ASCII value to character
+//            char character = (char) i;
+//            // Print character and its ASCII value
+//            System.out.println("Character: " + character + " ASCII value: " + i);
+//        }    OR
+
+//        for (int i = 0; i <= 127; i++) {
+//            // Convert ASCII value to character
+//            char character = (char) i;
+//            // Print character and its ASCII value
+//            System.out.println("ASCII value: " + i + " Character: " + character);
+//        }
+
+
+//        Need the output of the String by ASCII values
+//        String input = "Hello, World!";
+//
+//        // Loop through each character in the string
+//        for (int i = 0; i < input.length(); i++) {
+//            char cha = input.charAt(i);
+//            int ascii = (int) cha;
+//            // Print character and its ASCII value
+//            System.out.println("Character: " + cha + " ASCII value: " + ascii);
+//        }
+
+//        write a program to reverse the given Digits
+//        Scanner num = new Scanner(System.in);
+//        System.out.println("Enter the number: ");
+//        int n = num.nextInt();
+//
+//        for (int r =0; n != 0; n /= 10){
+//            int dig = n % 10;
+//            r = r * 10 + dig;
+//        }
+//        System.out.println("r");
+
+        //Access the Elements of an array
+//        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+//        System.out.println(cars[1]);
+
+        //Change the array of element
+//        cars[2] = "Mustang";
+//        System.out.println(cars[2]);
+
+        //Find the length of an Array
+//        System.out.println(cars.length);
+
+        //output of all the elements of cars if want to print all the elements in Array
+//        for (int i = 0; i <cars.length; i++) {
+//            System.out.println(cars[i]);
+//        }
+
+//        for (int i=0; i<=10; i++){
+//            System.out.println(i);
+//        }
+//        for (int i =1; i<10; i++){
+//            if (i%2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+
+        //need output of even number by Array program
+//        int[] num = {10, 15, 8, 5, 18, 12, 20, 25};
+//        for (int i = 0; i < num.length; i++) {
+//            if (i%2 == 0){
+//                System.out.println(num[i]);
+//            }
+//        }
+    }
+}
---
+++ src/ex_Lab001/Prat002_Array.java
@@ -0,0 +1,95 @@
+package ex_Lab001;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Prat002_Array {
+    public static void main(String[] args) {
+
+        //need output of even number by Array program
+//        int[] num = {10, 15, 8, 5, 18, 12, 20, 25};
+//        for (int i = 0; i < num.length; i++) {
+//            if (i%2 == 0){
+//                System.out.println(num[i]);
+//            }
+//        }
+
+        //Assending order and print
+//        int[] num = {10, 15, 8, 5, 18, 12, 20, 25};
+//        for (int i = 0; i < num.length; i++) {
+//            System.out.println(num[i]);
+//            Arrays.sort(num);
+//        }
+//        System.out.println(Arrays.toString(num));
+
+        //Maximum of number
+//        int[] num = {10, 15, 8, 5, 18, 12, 20, 25};
+//        int max = 0;
+//        int min = 1;
+//        for (int i = 0; i<num.length; i++){
+//            if (num[i] > max){
+//                max = num[i];
+//            }
+//            if (min > num[i]){
+//                min = num[i];
+//
+//            }
+//        }
+//        System.out.println(max);
+//        System.out.println(min);
+
+        //Use input number and print AVG
+//        Scanner sc = new Scanner(System.in);
+//        int[] num = new int[3];
+//        System.out.println("Enter num 1");
+//        num[0] = sc.nextInt();
+//        System.out.println("Enter num 2");
+//        num[1] = sc.nextInt();
+//        System.out.println("Enter num 3");
+//        num[2] = sc.nextInt();
+//
+//        int avg = num[0] + num[1] + num[2]/3;
+//        System.out.println(avg);
+
+        //Assending and decending
+//        Integer[] array = {5, 3, 8, 1, 2, 7, 4, 6};
+//        Arrays.sort(array, Collections.reverseOrder());
+//        System.out.println(Arrays.toString(array));
+//
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+
+//        int[] num = {20, 22, 18, 35, 48, 26, 87, 70};
+//        Arrays.sort(num);
+//        System.out.println(Arrays.toString(num));
+//
+//        Arrays.sort(num);
+//        for (int i = 0; i < num.length / 2; i++) {
+//            int temp = num[i];
+//            num[i] = num[num.length - 1 - i];
+//            num[num.length - 1 - i] = temp;
+//        }
+//        System.out.println(Arrays.toString(num));
+
+        //To print the last element of array
+//        int num[] = {20, 22, 18, 35, 48, 26, 87, 70};
+//        int highestNumber= num[num.length-1];
+//        System.out.println(highestNumber);
+//        Arrays.sort(num);
+//        System.out.println(Arrays.toString(num));
+//        for (int i = 1; i < num.length; i++) {
+//
+//        }
+//        System.out.println(Arrays.stream(num).max()); //it works
+//        System.out.println(Arrays.stream(num).min()); //it works
+
+//        if (num.length>0){
+//            int last = num[num.length-1];
+//            System.out.println(last);
+//        }
+
+
+//        }
+//
+    }
+}
